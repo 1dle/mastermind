ef74a8328461172984c2bf4c539268f9b4b66b4b
Bence Deli <delibnc@gmail.com>
2020-04-08 12:03:45 +0200

    Game layout designed

--- app/src/main/java/hu/idkfa/mastermind/model/GameTable.kt
+++ app/src/main/java/hu/idkfa/mastermind/model/GameTable.kt
@@ -3,6 +3,11 @@ package hu.idkfa.mastermind.model
 import kotlin.properties.Delegates
 
 class GameTable {
+    /*
+    companion object {
+        val instance = GameTable()
+    }*/
+
     var row = 1
     var column = 1
 }
